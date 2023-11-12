cb6586a95d35b1636ad2b82a65aa98ca9fef5d57
yan_hf <yan_hf@neusoft.co.jp>
2023-11-12 19:54:29 +0900

    add document

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SpringMVCExample-01
 Spring MVC Example --- thymeleaf mybatis html jquery
+
+## About Thymeleaf Engine
+
+we can use thymeleaf to view customers.
+
+https://www.thymeleaf.org/doc/tutorials/3.0/thymeleafspring.html
+
+https://www.thymeleaf.org/doc/articles/springmvcaccessdata.html
+
+
+## About BootStrap 
+
+What is BootStrap
+Powerful, extensible, and feature-packed frontend toolkit. 
+Build and customize with Sass, utilize prebuilt grid system and components, 
+and bring projects to life with powerful JavaScript plugins.
+
+https://getbootstrap.com/docs/5.3/getting-started/introduction/
+
+## About JQuery
+
+jQuery is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers. With a combination of versatility and extensibility, jQuery has changed the way that millions of people write JavaScript.
+
+we use JQuery plguin [Data Tables] to show tables.
+
+a most of jquery to oprater DOM and Style
+
+https://api.jquery.com/
--- src/main/java/com/example/servingwebcontent/CustomerController.java
+++ src/main/java/com/example/servingwebcontent/CustomerController.java
@@ -13,12 +13,19 @@ import com.example.servingwebcontent.entity.Customer;
 import com.example.servingwebcontent.repository.CustomerMapper;
 import com.google.gson.Gson;
 
+/**
+ * This class represents a controller for managing customers. It handles HTTP requests and responses related to customers.
+ * The class includes methods for displaying a list of customers, displaying a table of customers, and returning a JSON formatted string representation of the list of customers.
+ */
 @Controller
 public class CustomerController {
 	
 	@Autowired
 	private CustomerMapper mapper;
 
+	/**
+	 * Represents a sequence of characters. In this case, it is used to store a list of customer names.
+	 */
 	@GetMapping("/customer")
 	@ResponseBody
 	public String hello() {
@@ -29,6 +36,9 @@ public class CustomerController {
 		return names;
 	}
 
+	/**
+	 * The String class represents character strings.
+	 */
 	@GetMapping("/list")
 	public String list(Model model) {
 		String names = "users";
@@ -38,16 +48,29 @@ public class CustomerController {
 		return "list";
 	}
 
+	/**
+	 * Returns a string representation of an object.
+	 *
+	 * @return a string representation of an object.
+	 */
 	@GetMapping("/table")
 	public String initTable(Model model) {
 	
 		return "table";
 	}
 
+	/**
+	 * Returns a JSON formatted string representation of the list of customers.
+	 *
+	 * @return a JSON formatted string representation of the list of customers.
+	 */
 	@GetMapping("/showTable")
 	@ResponseBody
 	public String showTable() {
 		Gson gson = new Gson(); 
+		/**
+		 * List of customers retrieved from the database.
+		 */
 		List<Customer> list = mapper.select(SelectDSLCompleter.allRows());
 		
 		// 将list以json格式返回
