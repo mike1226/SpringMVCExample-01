6c5dd950e8596fe03d31c4245eb459becebaca75
yan_hf <yan_hf@neusoft.co.jp>
2023-11-20 22:58:36 +0900

    ddl更新

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,30 @@
+DROP TABLE IF EXISTS public.customer;
+DROP TABLE IF EXISTS public.Province;
+DROP TABLE IF EXISTS public.Country;
+
+CREATE TABLE IF NOT EXISTS public.customer
+(
+    id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    username character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    phone_number character varying(11) COLLATE pg_catalog."default" NOT NULL,
+    post_code character varying(7) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT customer_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO customer VALUES ('001', 'user001', 'test.user.001@example.com', '12345678901', '1234567');
+INSERT INTO customer VALUES ('002', 'user002', 'test.user.002@example.com', '23456789012', '2345671');
+INSERT INTO customer VALUES ('003', 'user003', 'test.user.003@example.com', '34567890123', '3456712');
+INSERT INTO customer VALUES ('004', 'user004', 'test.user.004@example.com', '45678901234', '4567123');
+INSERT INTO customer VALUES ('005', 'user005', 'test.user.005@example.com', '56789012345', '5671234');
+INSERT INTO customer VALUES ('006', 'user006', 'test.user.006@example.com', '67890123456', '6712345');
+INSERT INTO customer VALUES ('007', 'user007', 'test.user.007@example.com', '78901234567', '7123456');
+INSERT INTO customer VALUES ('008', 'user008', 'test.user.008@example.com', '89012345678', '1234567');
+INSERT INTO customer VALUES ('009', 'user009', 'test.user.009@example.com', '90123456789', '2345671');
+INSERT INTO customer VALUES ('010', 'user010', 'test.user.010@example.com', '01234567890', '3456712');
+
+
+
 CREATE TABLE Country (
  MstCountryCD varchar(10) NOT NULL,
  MstCountryNanme varchar(20),
@@ -6,6 +33,7 @@ CREATE TABLE Country (
 );
 
 ALTER TABLE Country ADD CONSTRAINT PK_Country PRIMARY KEY (MstCountryCD);
+
 
 
 CREATE TABLE Province (
