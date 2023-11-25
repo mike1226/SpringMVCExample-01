fdb0c49bc1c9b1ba64bbc9ca87a5d34d79cd4c67
XUJIAYANG <Administrator@PGXIONG>
2023-11-25 17:03:24 +0900

    测试

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -8,7 +8,6 @@ public class ServingWebContentApplication {
 
     public static void main(String[] args) throws Exception  {
         SpringApplication.run(ServingWebContentApplication.class, args);
-        
     }
 
 }
